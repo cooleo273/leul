06b2f50c44e3e9f7094a8567597c6fa680fc2a22
cooleo273 <leulteferi273@gmail.com>
2025-08-20 19:06:27 +0300

    Implement code changes to enhance functionality and improve performance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import '@/resources/custom.css'
 
 import classNames from "classnames";
 
-import { Background, Column, Flex, Meta, opacity, SpacingToken } from "@once-ui-system/core";
+import { Background, Column, Flex, Meta, opacity, SpacingToken, LayoutProvider } from "@once-ui-system/core";
 import { Footer, Header, RouteGuard, Providers } from '@/components';
 import { baseURL, effects, fonts, style, dataStyle, home } from '@/resources';
 
@@ -96,7 +96,8 @@ export default async function RootLayout({
         />
       </head>
       <Providers>
-        <Column as="body" background="page" fillWidth style={{minHeight: "100vh"}} margin="0" padding="0" horizontal="center">
+        <LayoutProvider>
+          <Column as="body" background="page" fillWidth style={{minHeight: "100vh"}} margin="0" padding="0" horizontal="center">
           <Background
             position="fixed"
             mask={{
@@ -138,7 +139,7 @@ export default async function RootLayout({
               color: effects.lines.color,
             }}
           />
-          <Flex fillWidth minHeight="16" hide="s"/>
+          <Flex fillWidth minHeight="16" hide={true}/>
             <Header />
             <Flex
               zIndex={0}
@@ -155,7 +156,8 @@ export default async function RootLayout({
             </Flex>
             <Footer/>
           </Column>
-        </Providers>
+        </LayoutProvider>
+      </Providers>
       </Flex>
   );
 }
